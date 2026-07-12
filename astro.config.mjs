c19abf80e354d1079299014ca82188deb6dff1ba
Jarmo Halonen <freefeel15@gmail.com>
2026-07-12 17:34:30 +0200

    fix: add @astrojs/cloudflare adapter to match Cloudflare Workers project configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +9,5 @@ export default defineConfig({
   integrations: [
     sitemap(),
   ],
+  adapter: cloudflare(),
 });
